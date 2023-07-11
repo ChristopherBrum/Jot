571cb5b7d23700cae3a6836899cd75b08deb2e33
Christopher Brum <christopher.brum@gmail.com>
2023-07-11 16:45:44 -0700

    created frontend build and setup serving static files from backend

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,10 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-
 app.use(cors())
-
+app.use(express.static('build'))
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
-
 app.use((req, res, next) => {
   if (req.method === 'POST') {
     console.log('Request Body:', req.body);
